ad7f74377153671376462f60f94fcb2007936338
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-01-28 23:50:41 -0800

    add: context ThemeProvider

---
+++ src/contexts/ThemeContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, useEffect, useState, ReactNode } from 'react';
+import { THEMES } from '../constants';
+import {
+  getLocalStorageItem,
+  setLocalStorageItem,
+} from '../utils/localStorage';
+import { ThemeColors } from '../types/common';
+
+interface ThemeContextType {
+  currentTheme: ThemeColors;
+  changeTheme: (newTheme: ThemeColors) => void;
+}
+
+export const ThemeContext = createContext<ThemeContextType | undefined>(
+  undefined
+);
+
+interface Props {
+  children: ReactNode;
+}
+
+export function ThemeProvider({ children }: Props) {
+  function getTheme(): ThemeColors {
+    const themeFromLocalStorage: ThemeColors | null =
+      getLocalStorageItem('theme');
+
+    return themeFromLocalStorage || THEMES[0];
+  }
+
+  const [theme, setTheme] = useState<ThemeColors>(getTheme);
+
+  useEffect(() => {
+    document.body.style.background = theme.interface.primaryColor;
+  }, [theme]);
+
+  function changeTheme(newTheme: ThemeColors) {
+    setTheme(newTheme);
+    setLocalStorageItem('theme', newTheme);
+  }
+
+  return (
+    <ThemeContext.Provider value={{ currentTheme: theme, changeTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
