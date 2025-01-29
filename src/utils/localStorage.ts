afe06542cf2bce68b20c62c2705de5ae9038255f
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-01-28 23:51:56 -0800

    fix: local storage helpers

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -5,7 +5,7 @@ export function setLocalStorageItem<T>(key: string, value: T): void {
 export function getLocalStorageItem<T>(key: string): T | null {
   const item = localStorage.getItem(key);
 
-  return item ? (JSON.parse(item) as T) : null;
+  return item ? JSON.parse(item) : null;
 }
 
 export function removeLocalStorageItem(key: string): void {
