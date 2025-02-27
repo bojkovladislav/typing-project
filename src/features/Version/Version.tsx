058a3091f714a5c7b63c515b93c7a3667244ba06
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-02-26 23:53:26 -0800

    fix: version 0.0.6 (beta)

--- src/features/Version/Version.tsx
+++ src/features/Version/Version.tsx
@@ -7,7 +7,7 @@ function Version() {
     <p
       style={{ color: currentTheme.interface.selectedColor }}
       className="text-sm absolute left-10 bottom-10"
-    >{`version 0.0.5 (beta)`}</p>
+    >{`version 0.0.6 (beta)`}</p>
   );
 }
 
