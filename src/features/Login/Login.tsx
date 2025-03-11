1cd58dc6b4056c04ea243ab4e030b17bddff3817
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-03-11 15:51:19 -0700

    feat: Login page created

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -1,15 +1,21 @@
+import { UserAddOutlined } from '@ant-design/icons';
 import BaseForm from '../../components/ui/BaseForm/BaseForm';
+import FormTitle from '../../components/ui/FormTitle/FormTitle';
 
 function Login() {
   return (
-    <BaseForm
-      values={{
-        username: '',
-        email: '',
-        password: '',
-      }}
-      onSubmit={() => console.log('submitted')}
-    />
+    <div className="flex flex-col gap-2">
+      <FormTitle title="register" icon={<UserAddOutlined />} />
+
+      <BaseForm
+        values={{
+          username: '',
+          email: '',
+          password: '',
+        }}
+        onSubmit={() => console.log('submitted')}
+      />
+    </div>
   );
 }
 
