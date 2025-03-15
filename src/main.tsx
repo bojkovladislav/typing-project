dd8c108d0ded5302e62caa5d1f9b3717a4a40f95
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-03-14 22:30:32 -0700

    fix: integrate layout

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,17 @@ import './index.css';
 import { ThemeProvider } from './contexts/ThemeContext.tsx';
 import Profile from './pages/Profile/Profile.tsx';
 import Authorize from './pages/Authorize/Authorize.tsx';
+import Layout from './pages/Layout/Layout.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <ThemeProvider>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/authorize" element={<Authorize />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="authorize" element={<Authorize />} />
+          <Route path="profile" element={<Profile />} />
+        </Route>
       </Routes>
     </ThemeProvider>
   </BrowserRouter>
