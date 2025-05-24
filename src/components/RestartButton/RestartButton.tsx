9ce66876b82e6b95fd16df7b95386af27bc4eeee
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-05-24 13:40:14 -0700

    fix: restart button

--- src/components/RestartButton/RestartButton.tsx
+++ src/components/RestartButton/RestartButton.tsx
@@ -1,5 +1,7 @@
+import { ReloadOutlined } from '@ant-design/icons';
 import { useTheme } from '../../hooks/useTheme';
-import Button from '../ui/Button/Button';
+import { useHover } from '../../hooks/useHover';
+import { Popover } from 'antd';
 
 interface RestartButtonProps {
   action: () => void;
@@ -8,16 +10,26 @@ interface RestartButtonProps {
 
 function RestartButton({ action, tabIndex }: RestartButtonProps) {
   const { currentTheme } = useTheme();
+  const { isHovered, hoverProps } = useHover();
 
   return (
-    <Button
-      action={action}
-      text="Restart test"
-      customStyles="w-[150px] text-lg"
-      tabIndex={tabIndex}
-      fill
-      additionalStyles={{ color: currentTheme.interface.primaryColor }}
-    />
+    <Popover
+      content={<p style={{ color: currentTheme.text.neutral }}>Restart Test</p>}
+      placement="bottom"
+      color={currentTheme.interface.secondaryColor}
+      rootClassName="custom-restart-popover"
+    >
+      <ReloadOutlined
+        onClick={action}
+        tabIndex={tabIndex}
+        onMouseEnter={hoverProps.onMouseEnter}
+        onMouseLeave={hoverProps.onMouseLeave}
+        style={{
+          color: !isHovered ? currentTheme.text.neutral : 'white',
+          transition: `0.3s all`,
+        }}
+      />
+    </Popover>
   );
 }
 
