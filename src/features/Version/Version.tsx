a08a5104a9fa83b91a68c478741018bf417d9f4a
Vladyslav Boiko <bojko.vladislav16@gmail.com>
2025-09-18 23:11:23 -0700

    chore: version 5.0.1 (beta)

--- src/features/Version/Version.tsx
+++ src/features/Version/Version.tsx
@@ -1,14 +1,14 @@
 import { useTheme } from '../../hooks/useTheme';
 
 function Version() {
-  const { currentTheme } = useTheme();
+    const { currentTheme } = useTheme();
 
-  return (
-    <p
-      style={{ color: currentTheme.interface.selectedColor }}
-      className="text-sm"
-    >{`5.0.0 (beta)`}</p>
-  );
+    return (
+        <p
+            style={{ color: currentTheme.interface.selectedColor }}
+            className='text-sm'
+        >{`5.0.1 (beta)`}</p>
+    );
 }
 
 export default Version;
